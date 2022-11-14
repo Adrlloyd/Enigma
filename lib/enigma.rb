269e6d9ed8037083adcb4ee19e75daed1e5045f2
Alastair Lloyd <dex8056@aol.com>
2022-11-14 15:31:56 -0700

    test: Add code method and amend tests to include encryption outcomes

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,11 +6,12 @@ class Enigma < Shift
   include Cypher
 
   def encrypt(message, key = rand_keys, date = today)
+    message = message.downcase
     keys = the_keys(key)
     offsets = the_offsets(date)
     shifts = the_shifts(keys, offsets)
     { 
-      encryption: message.downcase,
+      encryption: code(message, shifts),
       key: key,
       date: date
     }
@@ -24,4 +25,34 @@ class Enigma < Shift
     }
   end
 
+  def code(message, shifts)
+    counter = 0
+    output = ''
+    message.chars.each do |character|
+      if !characters.include?(character)
+        output += character
+      elsif counter == 0
+        shifted_characters = characters.rotate(shifts[:A])
+        index_position = characters.find_index(character)
+        output += shifted_characters[index_position]
+        counter += 1
+      elsif counter == 1
+        shifted_characters = characters.rotate(shifts[:B])
+        index_position = characters.find_index(character)
+        output += shifted_characters[index_position]
+        counter += 1
+      elsif counter == 2
+        shifted_characters = characters.rotate(shifts[:C])
+        index_position = characters.find_index(character)
+        output += shifted_characters[index_position]
+        counter += 1
+      elsif counter == 3
+        shifted_characters = characters.rotate(shifts[:D])
+        index_position = characters.find_index(character)
+        output += shifted_characters[index_position]
+        counter = 0 
+      end
+    end 
+    output
+  end
 end
